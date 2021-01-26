72aec639c12ff4ef8699f2e8ed7f609abae5c10e
Vanessa Orellana <vorellana814@gmail.com>
2021-01-26 15:10:13 -0500

    creating routes and folders necessary in pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Pages/Projects';
+import Articles from './Pages/Articles';
+import About from './Pages/About';
+import Logo from './img/vLogo.jpg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Routes  */}
+      <Route exact path="/" component={Projects} />
+      <Route path="/articles" component={Articles} />
+      <Route path="/about" component={About} />
+
+
+      <div className="navigation">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="">
+            <img src={Logo} width="60" height="60" class="d-inline-block align-top" alt="inital_logo" loading="lazy" />
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link" href="">Projects</a>
+              <a className="nav-link" href="">Articles</a>
+              <a className="nav-link" href="">About</a>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
