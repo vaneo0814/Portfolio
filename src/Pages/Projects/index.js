fe3ce6815f4ed892876d4f866325ac942d541af0
Vanessa Orellana <vorellana814@gmail.com>
2021-01-28 13:13:08 -0500

    added correct links

--- src/Pages/Projects/index.js
+++ src/Pages/Projects/index.js
@@ -40,19 +40,19 @@ function Projects() {
                 category="Website"
             />
             <Thumbnail
-                link="https://whispering-stream-30896.herokuapp.com/"
+                link="https://kaitlinkenney.github.io/SmartTrvlApp/"
                 image="https://i.postimg.cc/t7QzVCGr/Screen-Shot-2021-01-26-at-4-38-20-PM.png"
                 title="Smartrvl"
                 category="Website/App"
             />
             <Thumbnail
-                link="https://whispering-stream-30896.herokuapp.com/"
+                link="https://stormy-anchorage-68149.herokuapp.com/?id=600366bffc469a0015c0860c"
                 image="https://i.postimg.cc/1X6jdd9j/workoutdashboard.png"
                 title="Workout Tracker"
                 category="App"
             />
             <Thumbnail
-                link="https://whispering-stream-30896.herokuapp.com/"
+                link="https://vaneo0814.github.io/workdayPlanner/"
                 image="https://i.postimg.cc/Dw5YRLTn/workdayplanner.png"
                 title="Workday Planner"
                 category="App"
@@ -64,31 +64,31 @@ function Projects() {
                 category="App"
             />
             <Thumbnail
-                link="https://whispering-stream-30896.herokuapp.com/"
+                link=""
                 image="https://i.postimg.cc/dt6B0FS9/Screen-Shot-2021-01-26-at-4-36-22-PM.png"
                 title="Deep Dive BJJ"
-                category="Website"
+                category="Website (Work in progress...)"
             />
             <Thumbnail
-                link="https://whispering-stream-30896.herokuapp.com/"
+                link="https://vaneo0814.github.io/WebAPI_CodeQuiz/"
                 image="https://i.postimg.cc/vTMrQ2rj/Screen-Shot-2021-01-26-at-4-40-06-PM.png"
                 title="Gaming Quiz"
                 category="Website"
             />
             <Thumbnail
-                link="https://whispering-stream-30896.herokuapp.com/"
+                link="https://vaneo0814.github.io/NYT-Search/"
                 image="https://i.postimg.cc/8C6MLgw9/Screen-Shot-2021-01-26-at-4-39-25-PM.png"
                 title="New York Times Search"
                 category="Website"
             />
             <Thumbnail
-                link="https://whispering-stream-30896.herokuapp.com/"
+                link="https://github.com/vaneo0814/NoteTaskApp"
                 image="https://i.postimg.cc/kXGVxdgs/notetaker.png"
-                title="Notes"
-                category="App"
+                title="NoteTaker"
+                category="Express backend App"
             />
             <Thumbnail
-                link="https://whispering-stream-30896.herokuapp.com/"
+                link="https://vaneo0814.github.io/react_directory/"
                 image="https://i.postimg.cc/KYQqNYk4/Screen-Shot-2021-01-27-at-11-42-41-AM.png"
                 title="Employee Directory"
                 category="React App"
