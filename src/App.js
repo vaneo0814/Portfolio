d0aea2f31605c0ea44a8c6ecc920a2fe25d48a45
vaneo0814 <vorellana814@gmail.com>
2021-10-10 22:24:51 -0400

    fixed url

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <div className="App">
       <Navigation/>
       {/* Routes  */}
-      <Route exact path="/" component={Home} />
+      <Route exact path="/portfolio" component={Home} />
       <Route exact path="/projects" component={Projects} />
       <Route path="/about" component={About} />
     <Footer/>
